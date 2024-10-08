206e038d10c06065141ab83599ab81b8de2fbdc9
BlskMax <alejandro8g8@gmail.com>
2024-10-08 01:51:19 -0600

    vistas creadas

--- front/src/app/sound/page.tsx
+++ front/src/app/sound/page.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import styles from "@/components/backgrounds/Backgrounds.module.css"
 
-export default function Home() {
+export default function music () {
   return (
     <main 
     className={styles.fondo}>
 
     <div className="w-screen h-screen">
-      <h1 className="mt-[28vh] text-center text-8xl">Esta es la pagina de home</h1>
+      <h1 className="mt-[28vh] text-center text-8xl">Esta es la pagina de musica</h1>
     </div>  
 
     </main>
--- front/src/components/navbar/Navbar.tsx
+++ front/src/components/navbar/Navbar.tsx
@@ -22,24 +22,32 @@ function Navbar() {
     return (
 <main>
         <div className="absolute top-0 flex flex-col md:flex-row w-full items-center p-4 md:space-x-8 justify-between">
-            <section className="flex justify-start w-full md:w-auto items-center md:space-x-10">
-                <button type="button" onClick={() => router.push('/')}>
-                    <img src="/images/ccLogo.png" alt="logo" className="h-16 md:h-24 logo duration-500 hover:scale-105 cursor-pointer" />
-                </button>
-            </section>
+        <section className="flex justify-start w-full md:w-auto items-center md:space-x-10">
+    <button type="button" onClick={() => router.push('/')}>
+        <div className="relative h-16 md:h-[10vh] w-16 md:w-[10vh]">
+            <img src="/images/pvBasic.png" alt="logo" className="absolute h-full w-full logo duration-500 hover:scale-105 cursor-pointer" />
+            <img src="/images/pvBasicPurple.png" alt="logo-hover" className="absolute h-full w-full opacity-0 hover:opacity-100 transition-opacity duration-300" />
+        </div>
+    </button>
+        </section>              
+
 
             <section className="flex flex-row justify-end w-full md:w-auto space-x-4 md:space-x-12">
 
 
-                    <button type="button" onClick={() => router.push('/login')}>
-                        <h1 className={`${allura.variable} font-sans login cursor-pointer text-4xl md:text-5xl text-purple-700 hover:text-white transition-all custom-transition duration-300`}>Música</h1>
+                    <button type="button" onClick={() => router.push('/sound')}>
+                        <h1 className={`${allura.variable} font-sans login cursor-pointer text-4xl md:text-5xl text-purple-700 hover:text-white transition-all custom-transition duration-300`}>Sound</h1>
                     </button>
 
-                    <button type="button" onClick={() => router.push('/login')}>
-                        <h1 className={`${allura.variable} font-sans login cursor-pointer text-4xl md:text-5xl text-purple-700 hover:text-white transition-all custom-transition duration-300`}>Tienda</h1>
+                    <button type="button" onClick={() => router.push('/tickets')}>
+                        <h1 className={`${allura.variable} font-sans login cursor-pointer text-4xl md:text-5xl text-purple-700 hover:text-white transition-all custom-transition duration-300`}>Tickets</h1>
+                    </button>
+ 
+                    <button type="button" onClick={() => router.push('/store')}>
+                        <h1 className={`${allura.variable} font-sans login cursor-pointer text-4xl md:text-5xl text-purple-700 hover:text-white transition-all custom-transition duration-300`}>Store</h1>
                     </button>
 
-                    <button type="button" onClick={() => router.push('/login')}>
+                    <button type="button" onClick={() => router.push('/about')}>
                         <h1 className={`${allura.variable} font-sans login cursor-pointer text-4xl md:text-5xl text-purple-700 hover:text-white transition-all custom-transition duration-300`}>About</h1>
                     </button>
 
