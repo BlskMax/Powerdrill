af291c8d5a81f875ec9f4e9220491979b6be2a8a
BlskMax <alejandro8g8@gmail.com>
2025-02-11 19:23:39 -0600

    navbar actualizada

--- front/src/components/navbar/Navbar.tsx
+++ front/src/components/navbar/Navbar.tsx
@@ -21,45 +21,76 @@ function Navbar() {
 
     return (
 <main>
-        <div className="bg-black absolute top-0 flex flex-row md:flex-row w-full items-center md:p-2 p-[1vw] md:space-x-8 justify-between border-b-2 border-purple-700">
+    <div className="bg-black absolute top-0 flex flex-row md:flex-row w-full items-center md:p-2 p-[1vw] md:space-x-8 justify-between border-b-2 border-purple-700">
+        
+        {/* Logo */}
         <section className="flex justify-start w-full md:w-auto items-center md:space-x-10">
-    <button type="button" onClick={() => router.push('/')}>
-        <div className="relative h-8 md:h-[8vh] w-16 md:w-[8vh] md:mt-[1vh] md:mb-[1vh] ">
-            <img src="/images/pvBasic.png" alt="logo" className="md:absolute md:h-full md:w-full w-[10vw] logo duration-500 hover:scale-105 cursor-pointer" />
-            <img src="/images/pvBasicPurple.png" alt="logo-hover" className="md:absolute md:h-full md:w-full w-[10vw]  opacity-0 hover:opacity-100 transition-opacity duration-300" />
-        </div>
-    </button>
-        </section>              
+            <button type="button" onClick={() => router.push('/')}>
+                <div className="relative h-8 md:h-[8vh] w-16 md:w-[8vh] md:mt-[1vh] md:mb-[1vh]">
+                    <img src="/images/pvBasic.png" alt="logo" className="md:absolute md:h-full md:w-full w-[10vw] logo duration-500 hover:scale-105 cursor-pointer" />
+                    <img src="/images/pvBasicPurple.png" alt="logo-hover" className="md:absolute md:h-full md:w-full w-[10vw] opacity-0 hover:opacity-100 transition-opacity duration-300" />
+                </div>
+            </button>
+        </section>
 
+        {/* Botones */}
+        <section className="md:flex md:flex-row items-center w-[100vw] md:w-auto space-x-[4vw] md:space-x-12 md:pr-[1vw]">
 
-            <section className="md:flex md:flex-row justify-end w-[100vw] md:w-auto space-x-[4vw] md:space-x-12 md:pr-[1vw]">
+            {pathname !== '/sound' && (                        
+                <button type="button" onClick={() => router.push('/sound')}>
+                    <div className="relative w-[20vw] md:w-[10vw] h-[5vh] md:h-[8vh] group">
+                        <img 
+                            src="/images/soundButt.png" 
+                            alt="Sound" 
+                            className="absolute top-0 left-0 w-full h-full object-contain md:opacity-100 opacity-0 group-hover:opacity-0 transition-opacity duration-300"
+                        />
+                        <img 
+                            src="/images/soundGlow.png" 
+                            alt="Sound Hover" 
+                            className="absolute top-0 left-0 w-full h-full object-contain md:opacity-0 opacity-100 group-hover:opacity-100 transition-opacity duration-300"
+                        />
+                    </div>
+                </button>
+            )}
 
-                    {pathname !== '/sound' && (                        
-                    <button type="button" onClick={() => router.push('/sound')}>
-                        <h1 className={`${allura.variable} font-sans login cursor-pointer text-xl md:text-5xl text-white hover:text-pink-500 transition-all custom-transition duration-300`}>Sound</h1>
-                    </button>
-                    )}
+            {pathname !== '/tickets' && (
+                <button type="button" onClick={() => router.push('/tickets')}>
+                    <div className="relative w-[20vw] md:w-[10vw] h-[5vh] md:h-[8vh] group">
+                        <img 
+                            src="/images/showButt.png" 
+                            alt="Tickets" 
+                            className="absolute top-0 left-0 w-full h-full object-contain md:opacity-100 opacity-0 group-hover:opacity-0 transition-opacity duration-300"
+                        />
+                        <img 
+                            src="/images/showGlow.png" 
+                            alt="Tickets Hover" 
+                            className="absolute top-0 left-0 w-full h-full object-contain md:opacity-0 opacity-100 group-hover:opacity-100 transition-opacity duration-300"
+                        />
+                    </div>
+                </button>
+            )}
 
-                    {pathname !== '/tickets' && (
-                    <button type="button" onClick={() => router.push('/tickets')}>
-                        <h1 className={`${allura.variable} font-sans login cursor-pointer text-xl md:text-5xl text-white hover:text-pink-500 transition-all custom-transition duration-300`}>Shows</h1>
-                    </button>
-                    )}
+            {/* {pathname !== '/store' && (
+                <button type="button" onClick={() => router.push('/store')}>
+                    <div className="relative w-[20vw] md:w-[10vw] h-[5vh] md:h-[8vh] group">
+                        <img 
+                            src="/images/merchButt.png" 
+                            alt="Store" 
+                            className="absolute top-0 left-0 w-full h-full object-contain opacity-100 group-hover:opacity-0 transition-opacity duration-300"
+                        />
+                        <img 
+                            src="/images/merchGlow.png" 
+                            alt="Store Hover" 
+                            className="absolute top-0 left-0 w-full h-full object-contain opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+                        />
+                    </div>
+                </button>
+            )} */}
 
-                    {pathname !== '/store' && (
-                    <button type="button" onClick={() => router.push('/store')}>
-                        <h1 className={`${allura.variable} font-sans login cursor-pointer text-xl md:text-5xl text-white hover:text-pink-500 transition-all custom-transition duration-300`}>Merch</h1>
-                    </button>
-                    )}
-                    {/* {pathname !== '/exclusive' && (    
-                    <button type="button" onClick={() => router.push('/exclusive')}>
-                        <h1 className={`${allura.variable} font-sans login cursor-pointer text-xl md:text-5xl text-white hover:text-purple-700 transition-all custom-transition duration-300`}>Xclusive</h1>
-                    </button>
-                    )} */}
-
-            </section>
-        </div>
+        </section>
+    </div>
 </main>
+
 
     );
 }
--- front/src/components/songCard/SongCard.tsx
+++ front/src/components/songCard/SongCard.tsx
@@ -31,7 +31,7 @@ function SongCard({ song }: IsongProps) {
                         className="md:w-[36vw] md:h-[38vh] w-[36vw] h-[18vh] object-cover duration-300 hover:opacity-70"
                     />
 
-                    <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 hover:opacity-100 duration-300 transition-opacity text-white">
+                    <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-25 opacity-0 hover:opacity-100 duration-300 transition-opacity text-white">
                         <button
                             onClick={handleSongClick}
                             className="font-bold md:text-4xl hover:scale-110 duration-300"
@@ -39,7 +39,7 @@ function SongCard({ song }: IsongProps) {
                             <img
                             src="/images/adlerDetails.png"
                             alt="DETALLES"
-                            className="md:w-[12vw] opacity-80 hover:opacity-100 duration-300"
+                            className="md:w-[12vw] w-[24vw] opacity-80 hover:opacity-100 duration-300"
                             />
                         </button>
                     </div>
